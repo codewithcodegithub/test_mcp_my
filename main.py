d39956b3758d5cb1324e1c293f5ad2f61770b583
hlatif487 <hlatif487@gmail.com>
2025-12-27 20:01:52 +0500

    your message here1

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-from langchain_community.tools import DuckDuckGoSearchRun
+# from langchain_community.tools import DuckDuckGoSearchRun
 from fastmcp import FastMCP
 
 mcp = FastMCP(name='Demo Server')
 
 @mcp.tool
-def search_online_tool(query: str):
-    """Search the web."""
-    response =  DuckDuckGoSearchRun().invoke(query)
-    return response
+def sum(a: int,b: int):
+    """Add two number"""
+    result = a + b
+    return result
+
+# @mcp.tool
+# def search_online_tool(query: str):
+#     """Search the web."""
+#     response =  DuckDuckGoSearchRun().invoke(query)
+#     return response
 
 if __name__ == "__main__":
     mcp.run(transport= "http", host= "0.0.0.0", port=8000)
